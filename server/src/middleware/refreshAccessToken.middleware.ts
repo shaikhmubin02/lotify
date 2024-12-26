c18ffac92fdc161e61697627dcd607a690a42823
Mubin <shaikhmubin572@gmail.com>
2024-12-26 17:13:15 +0530

    init

--- server/src/middleware/refreshAccessToken.middleware.ts
+++ server/src/middleware/refreshAccessToken.middleware.ts
@@ -4,39 +4,47 @@ import axios from "axios";
 //middlerware for refreshing access token
 export const refreshAccessToken = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
+        const { token } = req.cookies;
 
-        //getting token from cookies
-        const token = req.cookies; //conditional check
-
-        //if there is token then call next
+        // If there is a token, proceed to next middleware
         if (token) {
             return next();
         }
-        // if there is not token then 
-        // using URLSearchParams for giving url encoded form data
-        const response = await axios.post("https://accounts.spotify.com/api/token", {
-            grant_type: "client_credentials",
-            client_id: process.env.CLIENT_ID,
-            client_secret: process.env.CLIENT_SECRET
-        },
-                {
+
+        // If no token, get a new one
+        const params = new URLSearchParams();
+        params.append('grant_type', 'client_credentials');
+        params.append('client_id', process.env.CLIENT_ID || '');
+        params.append('client_secret', process.env.CLIENT_SECRET || '');
+
+        const response = await axios.post("https://accounts.spotify.com/api/token", 
+            params,
+            {
                 headers: {
-                    "Content-Type": "application/x-www-form-urlencoded"
+                    'Content-Type': 'application/x-www-form-urlencoded'
                 }
             }
         );
 
-        console.log("resfreshing token...");
+        // Set the token in cookie and proceed
+        res.cookie("token", response.data.access_token, { 
+            maxAge: 58*60*1000, 
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+            domain: '.vercel.app'
+        });
 
+        // Store token in request for this request cycle
+        req.cookies.token = response.data.access_token;
+        
+        return next();
 
-        //sending response
-        res
-            .status(200)
-            .cookie("token", response.data.access_token, { httpOnly: true, maxAge: 58 * 60 * 1000 }) // max age defines expiry
-            .json({ message: "token refreshed", AccessToken: token });
     } catch (error) {
-        console.log(error);
-        // if res dosent send header then gives error 
-        res.status(500).json({ message: "refreshAccessToken middleware error", error });
+        console.log("refreshAccessToken error:", error);
+        return res.status(500).json({ 
+            message: "Error refreshing access token", 
+            error: error instanceof Error ? error.message : 'Unknown error' 
+        });
     }
 }
